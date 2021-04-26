8e17879bedc942be3dbd95a791581f69c9b4e0fc
Wout Peters <wout.peters@concapps.nl>
2021-04-26 15:52:38 +0200

    update dependencies

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -1,20 +1,20 @@
 // import Popper
-//=include ../../../node_modules/@popperjs/core/dist/umd/popper.js
+///=include ../../../node_modules/@popperjs/core/dist/umd/popper.js
 
 // import required js-files Bootstrap 5
-//=include ../../../node_modules/bootstrap/js/dist/alert.js
-//=include ../../../node_modules/bootstrap/js/dist/base-component.js
-//=include ../../../node_modules/bootstrap/js/dist/button.js
-//=include ../../../node_modules/bootstrap/js/dist/carousel.js
-//=include ../../../node_modules/bootstrap/js/dist/collapse.js
-//=include ../../../node_modules/bootstrap/js/dist/dropdown.js
-//=include ../../../node_modules/bootstrap/js/dist/modal.js
-//=include ../../../node_modules/bootstrap/js/dist/offcanvas.js
-//=include ../../../node_modules/bootstrap/js/dist/popover.js
-//=include ../../../node_modules/bootstrap/js/dist/scrollspy.js
-//=include ../../../node_modules/bootstrap/js/dist/tab.js
-//=include ../../../node_modules/bootstrap/js/dist/toast.js
-//=include ../../../node_modules/bootstrap/js/dist/tooltip.js
+///=include ../../../node_modules/bootstrap/js/dist/alert.js
+///=include ../../../node_modules/bootstrap/js/dist/base-component.js
+///=include ../../../node_modules/bootstrap/js/dist/button.js
+///=include ../../../node_modules/bootstrap/js/dist/carousel.js
+///=include ../../../node_modules/bootstrap/js/dist/collapse.js
+///=include ../../../node_modules/bootstrap/js/dist/dropdown.js
+///=include ../../../node_modules/bootstrap/js/dist/modal.js
+///=include ../../../node_modules/bootstrap/js/dist/offcanvas.js
+///=include ../../../node_modules/bootstrap/js/dist/popover.js
+///=include ../../../node_modules/bootstrap/js/dist/scrollspy.js
+///=include ../../../node_modules/bootstrap/js/dist/tab.js
+///=include ../../../node_modules/bootstrap/js/dist/toast.js
+///=include ../../../node_modules/bootstrap/js/dist/tooltip.js
 
 // Import other js-files
 //=include app.js
